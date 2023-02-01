cc57311a7ce3b7d4dc68d0dea883cd0c6cf4218e
anna.lezhnina <anna.lezhnina@travelline.ru>
2023-02-01 10:22:57 +0300

    Реализован компонент MeetupView

---
+++ 02-components/06-MeetupView/components/MeetupDescription.js
@@ -0,0 +1,14 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupDescription',
+
+  props: {
+    description: {
+      type: String,
+      default: 'Описание должно быть здесь',
+    },
+  },
+
+  template: `<p class="meetup-description">{{ description }}</p>`,
+});
