4c6612bc55e12f8831af813415401b4c595da45d
anna.lezhnina <anna.lezhnina@travelline.ru>
2023-02-06 13:24:10 +0300

    Реализован компонент TheToaster

---
+++ 04-vue-cli/02-TheToaster/components/TheToast.vue
@@ -0,0 +1,83 @@
+<template>
+  <div v-if="shown" :class="`toast toast_${type}`">
+    <ui-icon class="toast__icon" :icon="$options.icons[type]"/>
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon.vue';
+
+const icons = {
+  success: 'check-circle',
+  error: 'alert-circle',
+};
+
+export default {
+  name: 'TheToast',
+
+  icons,
+
+  components: {UiIcon},
+
+  props: {
+    type: {
+      type: String,
+      default: 'success'
+    },
+    text: {
+      type: String,
+    },
+    timeout: {
+      type: Number,
+      default: 5000,
+    },
+  },
+
+  data() {
+    return {
+      shown: null,
+    }
+  },
+
+  created() {
+    this.shown = true;
+
+    setTimeout(() => {
+      this.shown = false;
+    }, this.timeout);
+  }
+};
+</script>
+
+<style scoped>
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast + .toast {
+  margin-top: 20px;
+}
+
+.toast__icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
+}
+</style>
--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <TheToast v-for="(toast, index) in toasts" :key="index" :type="toast.type" :text="toast.text" :timeout="toast.timeout" />
   </div>
 </template>
 
 <script>
 import UiIcon from './UiIcon.vue';
+import TheToast from './TheToast';
 
 export default {
   name: 'TheToaster',
 
-  components: { UiIcon },
+  components: {
+    TheToast,
+    UiIcon
+  },
+
+  data() {
+    return {
+      toasts: []
+    }
+  },
+
+  methods: {
+    success: function (text) {
+      this.toasts.push({type: 'success', text: text});
+    },
+    error: function (text) {
+      this.toasts.push({type: 'error', text: text});
+    }
+  }
 };
 </script>
 
@@ -39,35 +50,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
