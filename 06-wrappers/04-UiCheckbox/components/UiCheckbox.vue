0d5910ad36cb7083259da6c47d3ae56bfb07ab08
anna.lezhnina <anna.lezhnina@travelline.ru>
2023-02-20 15:39:24 +0300

    Реализован компонент UiCheckbox

--- 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
+++ 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
@@ -1,14 +1,32 @@
 <template>
   <label class="checkbox">
-    <input type="checkbox" class="checkbox__input" />
+    <input type="checkbox" class="checkbox__input" v-bind="$attrs" :value="value" v-model="customModel" />
     <span class="checkbox__box"></span>
-    Label Text
+    <slot />
   </label>
 </template>
 
 <script>
 export default {
   name: 'UiCheckbox',
+
+  inheritAttrs: false,
+
+  props: {
+    modelValue: [Boolean, Array, Set],
+    value: String,
+  },
+
+  computed: {
+    customModel: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
 };
 </script>
 
