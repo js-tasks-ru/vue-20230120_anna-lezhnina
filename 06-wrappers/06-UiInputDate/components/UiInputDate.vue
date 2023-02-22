e02faf37f608f7ae8df4493c2d105126ee269a0e
anna.lezhnina <anna.lezhnina@travelline.ru>
2023-02-22 10:50:54 +0300

    Реализован компонент UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,57 @@
 <template>
-  <UiInput />
+  <UiInput :modelValue="value" v-bind="$attrs" :type="type" @change="onChange">
+    <template v-for="slotName in Object.keys($slots)" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
 import UiInput from './UiInput.vue';
+import moment from 'moment';
 
 export default {
   name: 'UiInputDate',
 
   components: { UiInput },
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+    },
+    modelValue: {
+      type: String,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    value() {
+      if (!this.modelValue) {
+        return '';
+      }
+
+      const utcDate =  moment(this.modelValue).utc();
+
+      switch (this.type) {
+        case 'time':
+          return utcDate.format('HH:mm');
+        case 'datetime-local':
+          return utcDate.format('YYYY-MM-DDTHH:mm');
+        case 'date':
+          return utcDate.format('YYYY-MM-DD');
+        default:
+          return '';
+      }
+    },
+  },
+
+  methods: {
+    onChange(e) {
+      this.$emit('update:modelValue', e.target.valueAsNumber);
+    },
+  },
 };
 </script>
