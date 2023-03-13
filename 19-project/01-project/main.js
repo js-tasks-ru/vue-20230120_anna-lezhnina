4366c3a53593c27216ddc4b1b44b263fce33383b
anna.lezhnina <anna.lezhnina@travelline.ru>
2023-03-13 18:32:20 +0300

    Добавлен репозиторий проекта

--- 19-project/01-project/main.js
+++ 19-project/01-project/main.js
@@ -1,2 +1,2 @@
 // Укажите в качестве значения переменной ссылку на репозиторий с вашим проектом
-export const REPOSITORY_URL = '';
+export const REPOSITORY_URL = 'https://github.com/lezhnina-anna/vue-project/tree/master/my-vue-app';
